fa96859cce5f939771a26c88b0f24a3f4c02a487
Azmir <azmirhasan.az2@gmail.com>
2023-12-08 23:03:54 +0600

    Payload Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import MaxWidthWrapper from '@/components/MaxWidthWrapper';
-import { Button, buttonVariants } from '@/components/ui/button';
-import {
-  ArrowDownToLine,
-  CheckCircle,
-  Leaf
-} from 'lucide-react';
+import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react';
 import Link from 'next/link';
+import MaxWidthWrapper from '../components/MaxWidthWrapper';
+import { Button, buttonVariants } from '../components/ui/button';
 
 type Props = {};
 
